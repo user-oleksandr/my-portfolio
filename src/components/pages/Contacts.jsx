4de4a1dd3347b8e8c54e295df3f004df5683a25e
Oleksandr Anastasiiev <name.oleksandr@gmail.com>
2023-06-25 12:00:25 +0300

    style for contact

--- src/components/pages/Contacts.jsx
+++ src/components/pages/Contacts.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 function Contact() {
     return (
-        <div className="container bg-dark rounded" style={{marginTop: '150px', padding: '50px'}}>
+        <div className="container bg-dark rounded mb-5" style={{marginTop: '150px', padding: '50px'}}>
             <div className='row text-center text-white'>
                 <h2>Contact Me</h2>
             </div>
             <div className="row mt-5">
-                <div className="col">
+                <div className="col-lg-6 ">
                     <p className='text-white'>You can also contact me via email:</p>
                     <ul className="link-list" style={{listStyle: 'none', padding: '0'}}>
                         <li>
@@ -23,7 +23,7 @@ function Contact() {
                         </li>
                     </ul>
                 </div>
-                <div className="col">
+                <div className="col-lg-6">
                     <p className='text-white'>You can reach out to me through the following channels:</p>
                     <ul className="link-list" style={{listStyle: 'none', padding: '0'}}>
                         <li>
